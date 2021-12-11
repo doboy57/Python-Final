c3228268c0e91d05fe8f5a3f8ce863c61a9fab0c
Ryan Do <ryan_do1@baylor.edu>
2021-12-11 17:11:27 -0600

    pre reformatttinh

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Pizza(models.Model):
     name = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Pizza, Toppings, Comment
 from .forms import PizzaForm, ToppingForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 
@@ -12,23 +14,26 @@ def index(request):
         "pizzas/index.html",
     )
 
-
+@login_required
 def pizzas(request):
-    pizzas = Pizza.objects.order_by("date")
+    pizzas = Pizza.objects.filter(owner=request.user).order_by("date")
 
     context = {"pizzas": pizzas}
     return render(request, "pizzas/pizzas.html", context)
 
-
+@login_required
 def pizza(request, pizza_id):
     pizza = Pizza.objects.get(id=pizza_id)
+    if pizza.owner != request.user:
+        raise Http404
     toppings = pizza.toppings_set.order_by("-date_added")
     comments = pizza.comment_set.order_by('-date_added')
 
     context = {"pizza": pizza, "toppings": toppings, 'comments': comments}
     return render(request, "pizzas/pizza.html", context)
 
-
+    
+@login_required
 def new_pizza(request):
     if request.method != "POST":
         form = PizzaForm()
@@ -39,16 +44,18 @@ def new_pizza(request):
         if form.is_valid():
             new_pizza = form.save(commit=False)
             new_pizza.owner = request.user
-            form.save()
+            new_pizza.save()
 
             return redirect("pizzas:pizzas")
     context = {"form": form}
     return render(request, "pizzas/new_pizza.html", context)
 
-
+@login_required
 def new_topping(request, pizza_id):
     pizza = Pizza.objects.get(id=pizza_id)
-
+    
+    if pizza.owner != request.user:
+        raise Http404
     if request.method != "POST":
         form = ToppingForm()
 
@@ -58,7 +65,7 @@ def new_topping(request, pizza_id):
         if form.is_valid():
             new_topping = form.save(commit=False)
             new_topping.pizza = pizza
-
+            new_topping.owner = request.user
             new_topping.save()
 
             return redirect("pizzas:pizza", pizza_id=pizza_id)
@@ -66,10 +73,12 @@ def new_topping(request, pizza_id):
     context = {"form": form, "pizza": pizza}
     return render(request, "pizzas/new_topping.html", context)
 
-
+@login_required
 def edit_topping(request, topping_id):
     topping = Toppings.objects.get(id=topping_id)
     pizza = topping.pizza
+    if topping.owner != request.user:
+        raise Http404
 
     if request.method != "POST":
         form = ToppingForm(instance=topping)
@@ -83,6 +92,7 @@ def edit_topping(request, topping_id):
     context = {"topping": topping, "pizza": pizza, "form": form}
     return render(request, 'pizzas/edit_topping.html', context)
 
+@login_required
 def new_comment(request, pizza_id):
     pizza = Pizza.objects.get(id=pizza_id)
 
